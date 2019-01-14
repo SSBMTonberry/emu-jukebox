63566876f259d1238e5a114bdb725261b59b6e12
Robin Pettersen <ssbmtonberry@gmail.com>
2019-01-14 22:36:51 +0100

    Added clipboard-support, track-info, and previous/next track.
    Also, you can now load tracks from a file path.

---
+++ src/managers/ClipboardManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by robin on 14.01.19.
+//
+
+#include "ClipboardManager.h"
---
+++ src/managers/ClipboardManager.h
@@ -0,0 +1,15 @@
+//
+// Created by robin on 14.01.19.
+//
+
+#ifndef EMU_JUKEBOX_CLIPBOARDMANAGER_H
+#define EMU_JUKEBOX_CLIPBOARDMANAGER_H
+
+
+class ClipboardManager
+{
+
+};
+
+
+#endif //EMU_JUKEBOX_CLIPBOARDMANAGER_H
