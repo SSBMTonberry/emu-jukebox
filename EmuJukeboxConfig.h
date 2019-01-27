ebcf5501dfefc7ef53e6894f2800252c545dc344
Robin Pettersen <ssbmtonberry@gmail.com>
2019-01-27 16:09:26 +0100

    Added all OSX based libraries for SFML.

--- EmuJukeboxConfig.h
+++ EmuJukeboxConfig.h
@@ -1,13 +1,13 @@
 #define LINUX 0
-#define WINDOWS 1
-#define APPLE 0
-#define MSVC 1
+#define WINDOWS 0
+#define APPLE 1
+#define MSVC 0
 #define MINGW 0
-#define CMAKE_MAKE_PROGRAM nmake
-#define CMAKE_SYSTEM_NAME Windows
-#define CMAKE_HOST_SYSTEM_NAME Windows //Windows, Linux or Darwin (OS X)
-#define CMAKE_HOST_SYSTEM_PROCESSOR AMD64
-#define CMAKE_CXX_COMPILER_VERSION 19.16.27026.1
+#define CMAKE_MAKE_PROGRAM /usr/bin/make
+#define CMAKE_SYSTEM_NAME Darwin
+#define CMAKE_HOST_SYSTEM_NAME Darwin //Windows, Linux or Darwin (OS X)
+#define CMAKE_HOST_SYSTEM_PROCESSOR x86_64
+#define CMAKE_CXX_COMPILER_VERSION 7.0.1
 #define PROJECT_VERSION_MAJOR 0
 #define PROJECT_VERSION_MINOR 1
 #define PROJECT_VERSION_PATCH 0
