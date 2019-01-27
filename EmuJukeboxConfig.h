54610ed33c9a691b7c79c216d487a9fe1c2d205a
Robin Pettersen <ssbmtonberry@gmail.com>
2019-01-27 01:13:30 +0100

    Now compiles statically with MSVC, but requires openal.dll to work...
    Look at alternatives for this later. Also try to fix the MinGW-build

--- EmuJukeboxConfig.h
+++ EmuJukeboxConfig.h
@@ -1,13 +1,13 @@
 #define LINUX 0
 #define WINDOWS 1
 #define APPLE 0
-#define MSVC 0
-#define MINGW 1
-#define CMAKE_MAKE_PROGRAM C:/MinGW/mingw-w64/x86_64-7.3.0-posix-seh-rt_v5-rev0/mingw64/bin/mingw32-make.exe
+#define MSVC 1
+#define MINGW 0
+#define CMAKE_MAKE_PROGRAM nmake
 #define CMAKE_SYSTEM_NAME Windows
 #define CMAKE_HOST_SYSTEM_NAME Windows //Windows, Linux or Darwin (OS X)
 #define CMAKE_HOST_SYSTEM_PROCESSOR AMD64
-#define CMAKE_CXX_COMPILER_VERSION 7.3.0
+#define CMAKE_CXX_COMPILER_VERSION 19.16.27026.1
 #define PROJECT_VERSION_MAJOR 0
 #define PROJECT_VERSION_MINOR 1
 #define PROJECT_VERSION_PATCH 0
